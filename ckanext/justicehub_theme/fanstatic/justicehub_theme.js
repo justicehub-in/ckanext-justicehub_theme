b66707f3ed4521385369279e875bbdc69fc262c7
curioswati <swati@civicdatalab.in>
2020-06-26 21:30:31 +0530

    update partner detail page, add read more effect

--- ckanext/justicehub_theme/fanstatic/justicehub_theme.js
+++ ckanext/justicehub_theme/fanstatic/justicehub_theme.js
@@ -1,3 +1,38 @@
+// https://www.viralpatel.net/dynamically-shortened-text-show-more-link-jquery/
+$(document).ready(function() {
+	var showChar = 215;
+	var ellipsestext = "...";
+	var moretext = "more";
+	var lesstext = "less";
+	$('.more').each(function() {
+		var content = $(this).html();
+
+		if(content.length > showChar) {
+
+			var c = content.substr(0, showChar);
+			var h = content.substr(showChar-1, content.length - showChar);
+
+			var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+			$(this).html(html);
+		}
+	});
+
+	$(".morelink").click(function(){
+		if($(this).hasClass("less")) {
+			$(this).removeClass("less");
+			$(this).html(moretext);
+		} else {
+			$(this).addClass("less");
+			$(this).html(lesstext);
+		}
+		$(this).parent().prev().toggle();
+		$(this).prev().toggle();
+		return false;
+	});
+});
+
+
 $('[data-toggle="tooltip"]').tooltip();
 
 // https://stackoverflow.com/a/23006629/3860168
