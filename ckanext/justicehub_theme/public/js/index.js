ca902058a78f7fba631f83c9c13dbe8b17939b24
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-12-18 09:47:15 +0530

    added file icons, fixed the input values resettting issue

--- ckanext/justicehub_theme/public/js/index.js
+++ ckanext/justicehub_theme/public/js/index.js
@@ -20,17 +20,17 @@ import Dataset from './dataset.js';
     dataUploadSections.forEach((section) => {
       if (section.classList.contains(dataUploadSteps.activeStep.section)) {
         section.classList.remove('hide');
-      } else {
-        section.classList.add('hide');
+        return;
       }
+      section.classList.add('hide');
     });
 
     stepIndicators.forEach((step) => {
       if (parseInt(step.dataset.value) === dataUploadSteps.activeStep.id) {
         step.classList.add('active');
-      } else {
-        step.classList.remove('active');
+        return;
       }
+      step.classList.remove('active');
     });
   }
 
@@ -60,9 +60,36 @@ import Dataset from './dataset.js';
   });
 
   // file upload section
-  const fileInputs = document.querySelectorAll('input[type="file"]');
 
-  const addMoreFilesButton = document.querySelector('.add-more--files');
+  const FILEINPUT_ICONS = [
+    {
+      format: 'xls',
+      backgroundColor: '#CEE1CE',
+      iconTemplate: '<span class="iconify" data-icon="vscode-icons:file-type-excel2" data-inline="false"></span>'
+    },
+    {
+      format: 'pdf',
+      backgroundColor: '#F8BE67',
+      iconTemplate: '<span class="iconify" data-icon="icomoon-free:file-pdf" data-inline="false"></span>'
+    },
+    {
+      format: 'csv',
+      backgroundColor: '#F8BE67',
+      iconTemplate: '<span class="iconify" data-icon="grommet-icons:document-csv" data-inline="false"></span>'
+    }
+  ];
+
+  function getFileUploadBoxPropertyByFileType(fileType, property) {
+    return FILEINPUT_ICONS.find((file) => file.format === fileType)[property];
+  }
+
+  // function generateFileIconNode(iconName) {
+  //   return document
+  //     .createElement('span')
+  //     .setAttribute('class', 'iconify')
+  //     .setAttribute('data-icon', iconName)
+  //     .setAttribute('data-inline', 'false');
+  // }
 
   function generateFileUploadField(fileId) {
     return `
@@ -85,32 +112,67 @@ import Dataset from './dataset.js';
   `;
   }
 
+  const fileUploadContainer = document.querySelector('.data-upload-section__form__field--file');
+  const addMoreFilesButton = document.querySelector('.add-more--files');
+
   addMoreFilesButton.addEventListener('click', () => {
     const fileUploadForm = document.querySelector('.data-upload-section__form__field--file');
-    fileUploadForm.innerHTML = fileUploadForm.innerHTML + generateFileUploadField(Math.random());
+    fileUploadForm.insertAdjacentHTML('beforeend', generateFileUploadField(Math.random()));
   });
 
-  fileInputs.forEach((fileInput) => {
-    fileInput.addEventListener('change', autoPopulateFileName);
+  fileUploadContainer.addEventListener('change', (event) => {
+    // handle event bubbling
+    if (!event.target.matches('input[type="file"]')) {
+      return;
+    }
+    fileInputHandler(event);
   });
 
-  function autoPopulateFileName(event) {
+  function updateFileInputBackground(fileInputBackgroundElement, fileType) {
+    fileInputBackgroundElement.style.backgroundColor = getFileUploadBoxPropertyByFileType(fileType, 'backgroundColor');
+    fileInputBackgroundElement.removeChild(fileInputBackgroundElement.querySelector('svg'));
+    fileInputBackgroundElement.insertAdjacentHTML(
+      'beforeend',
+      getFileUploadBoxPropertyByFileType(fileType, 'iconTemplate')
+    );
+  }
+
+  function fileInputHandler(event) {
     const file = event.target.files[0];
-    const correspondingFileNameInput = this.parentElement.nextSibling.nextSibling.querySelector('input');
+    const fileInputBackground = event.target.parentElement;
+    fileInputBackground.style.border = '1px solid #524F4F';
+    if (file.type.indexOf('csv') > -1) {
+      updateFileInputBackground(fileInputBackground, 'csv');
+    } else if (file.type.indexOf('spreadsheetml') > -1) {
+      updateFileInputBackground(fileInputBackground, 'xls');
+    } else if (file.type.indexOf('pdf') > -1) {
+      updateFileInputBackground(fileInputBackground, 'pdf');
+    }
+    const correspondingFileNameInput = fileInputBackground.nextSibling.nextSibling.querySelector('input');
     correspondingFileNameInput.value = file.name;
   }
 
-  const proceedFromFilesButton = document.querySelector('#proceedFromFilesButton');
-  proceedFromFilesButton.addEventListener('click', () => {
+  function updateDatasetWithValuesFromFilesSection() {
     dataset.updateProperty('name', getValueFromInputSelector('#datasetNameField'));
     const fileUploads = document.querySelectorAll('.file-upload');
     fileUploads.forEach((file) => {
-      dataset.addItemToListProperty('files', getFileDetailsFromFileUploadElement(file));
+      if (getFileDetailsFromFileUploadElement(file).fileName) {
+        dataset.addItemToListProperty('files', getFileDetailsFromFileUploadElement(file));
+      }
     });
+  }
+
+  const saveOnFilesSectionButton = document.getElementById('saveOnFilesSectionButton');
+  saveOnFilesSectionButton.addEventListener('click', () => {
+    updateDatasetWithValuesFromFilesSection();
+    postDatasetRequest('http://localhost:5000', 'draft');
   });
 
+  const proceedFromFilesButton = document.querySelector('#proceedFromFilesButton');
+  proceedFromFilesButton.addEventListener('click', updateDatasetWithValuesFromFilesSection);
+
   // ownership section
-  const addAuthorsButton = document.querySelector('.add-more--authors');
+  const addMoreAuthorsButton = document.querySelector('.add-more--authors');
   function generateAuthorField(publisherType) {
     return `
     <div class="item-addition item-addition--author">
@@ -126,10 +188,10 @@ import Dataset from './dataset.js';
   `;
   }
 
-  addAuthorsButton.addEventListener('click', () => {
+  addMoreAuthorsButton.addEventListener('click', () => {
     const authorAdditionForm = document.querySelector('.data-upload-section__form__field--authors');
     const publisherType = getRadioValue('primary-publisher') === 'individual' ? 'Author' : 'Organization';
-    authorAdditionForm.innerHTML = authorAdditionForm.innerHTML + generateAuthorField(publisherType);
+    authorAdditionForm.insertAdjacentHTML('beforeend', generateAuthorField(publisherType));
   });
 
   const LICENSES = [
@@ -153,13 +215,12 @@ import Dataset from './dataset.js';
   let licenseOptions = '';
 
   LICENSES.forEach((license) => {
-    licenseOptions = licenseOptions + generateLicenseOptionHTML(license);
+    licenseOptions += generateLicenseOptionHTML(license);
   });
 
   licenseSelect.innerHTML = licenseOptions;
 
-  const proceedFromOwnershipButton = document.querySelector('#proceedFromOwnershipButton');
-  proceedFromOwnershipButton.addEventListener('click', () => {
+  function updateDatasetWithValuesFromOwnershipSection() {
     const selectedLicense = getValueFromInputSelector('#licenseSelect');
     const licenseDescription = LICENSES.find((license) => license.name === selectedLicense).description;
     dataset.updateProperty('license', {
@@ -174,7 +235,16 @@ import Dataset from './dataset.js';
       publisher.authors.push(getAuthorDetailsFromAuthorAdditionElement(author));
     });
     dataset.updateProperty('publisher', publisher);
+  }
+
+  const saveOnOwnershipSectionButton = document.getElementById('saveOnOwnershipSectionButton');
+  saveOnOwnershipSectionButton.addEventListener('click', () => {
+    updateDatasetWithValuesFromOwnershipSection();
+    postDatasetRequest('http://localhost:5000', 'draft');
   });
+
+  const proceedFromOwnershipButton = document.getElementById('proceedFromOwnershipButton');
+  proceedFromOwnershipButton.addEventListener('click', updateDatasetWithValuesFromOwnershipSection);
 
   // data relevancy section
   const regionRadioOptions = document.querySelectorAll(`input[name="region-options"]`);
@@ -232,23 +302,31 @@ import Dataset from './dataset.js';
     dropdown.innerHTML = monthOptions;
   });
 
-  const proceedFromRelevancyButton = document.querySelector('#proceedFromRelevancyButton');
-  proceedFromRelevancyButton.addEventListener('click', () => {
+  function updateDatasetWithValuesFromRelevancySection() {
     const language =
       getRadioValue('language-options') === 'other'
         ? getValueFromInputSelector('#otherLanguageInput')
         : getRadioValue('language-options');
     dataset.updateProperty('language', language);
-    const region = { country: getRadioValue('region-options'), subRegions: [] };
+    const region = [getRadioValue('region-options')];
     dataset.updateProperty('region', region);
     dataset.updateProperty('timePeriod', {
       from: getTimePeriodDetailsFromSelector('.period--from'),
       to: getTimePeriodDetailsFromSelector('.period--to')
     });
+  }
+
+  const saveOnRelevancySectionButton = document.getElementById('saveOnRelevancySectionButton');
+  saveOnRelevancySectionButton.addEventListener('click', () => {
+    updateDatasetWithValuesFromRelevancySection();
+    postDatasetRequest('http://localhost:5000', 'draft');
   });
 
-  // source of data section
-  const addReferencesButton = document.querySelector('.add-more--references');
+  const proceedFromRelevancyButton = document.getElementById('proceedFromRelevancyButton');
+  proceedFromRelevancyButton.addEventListener('click', updateDatasetWithValuesFromRelevancySection);
+
+  // sources of dataset section
+  const addMoreReferencesButton = document.querySelector('.add-more--references');
   function generateReferenceField(referenceId) {
     return `
     <div class="item-addition item-addition--reference" id="${referenceId}">
@@ -264,9 +342,9 @@ import Dataset from './dataset.js';
   `;
   }
 
-  addReferencesButton.addEventListener('click', () => {
+  addMoreReferencesButton.addEventListener('click', () => {
     const referenceAdditionForm = document.querySelector('.data-upload-section__form__field--references');
-    referenceAdditionForm.innerHTML = referenceAdditionForm.innerHTML + generateReferenceField(Math.random());
+    referenceAdditionForm.insertAdjacentHTML('beforeend', generateReferenceField(Math.random()));
   });
 
   function generateTagElement(tagName) {
@@ -305,13 +383,11 @@ import Dataset from './dataset.js';
 
   const keywordsContainer = document.getElementById('keywordsContainer');
   const sourcesContainer = document.getElementById('sourcesContainer');
-
   keywordsContainer.addEventListener('click', (event) => removeTag(event, 'keywords', keywordsContainer));
   sourcesContainer.addEventListener('click', (event) => removeTag(event, 'sources', sourcesContainer));
 
   const keywordsInput = document.getElementById('keywordsInput');
   const sourcesInput = document.getElementById('sourcesInput');
-
   keywordsInput.addEventListener('keyup', (event) => addTag(event, 'keywords'));
   sourcesInput.addEventListener('keyup', (event) => addTag(event, 'sources'));
 
@@ -375,6 +451,23 @@ import Dataset from './dataset.js';
       dataset.addItemToListProperty('referenceLinks', getReferenceLinksFromReferenceElement(reference));
     });
 
+    function generateFilePreviewHTML(fileName = '', fileDescription, icon = 'vscode-icons:file-type-excel2') {
+      return `
+        <div class="file-summary">
+          <div class="file-summary__icon-container">
+            <span class="iconify" data-icon="${icon}" data-inline="false"></span>
+          </div>
+          <div class="file-summary__text">
+            <h4>${fileName}</h4>
+            <div class="file-summary__text__description">
+              <p>Description:</p>
+              <p>${fileDescription}</p>
+            </div>
+          </div>
+        </div>
+      `;
+    }
+
     const dataRelevancyPreviewTableHTML = `
     <tr>
       <td>Region(s) covered:</td>
@@ -421,69 +514,89 @@ import Dataset from './dataset.js';
   `;
     // change the references links to get all the items
 
+    dataset.files.forEach((file) => {
+      document.querySelector('#filesListOnPreview').innerHTML =
+        document.querySelector('#filesListOnPreview').innerHTML +
+        generateFilePreviewHTML(file.fileName, file.fileDescription);
+    });
+
     document.querySelector('#dataRelevancyPreviewTable').innerHTML = dataRelevancyPreviewTableHTML;
     document.querySelector('#ownershipPreviewTable').innerHTML = ownershipPreviewTableHTML;
     document.querySelector('#sourcePreviewTable').innerHTML = sourcePreviewTableHTML;
-
     document.querySelector('#datasetNameOnPreviewModal').innerHTML = dataset.name;
 
     console.log(dataset);
   });
+
+  // preview modal
+
+  const editSectionButtons = document.querySelectorAll('.preview-edit');
+  editSectionButtons.forEach((button) =>
+    button.addEventListener('click', () => {
+      dataUploadSteps.setActiveStepByName(button.dataset.section, updateActiveSectionDOM);
+    })
+  );
 
   const submitDatasetButton = document.getElementById('submitDatasetButton');
   submitDatasetButton.addEventListener('click', () => {
     postDatasetRequest('http://localhost:5000');
   });
 
-  const saveAsDatasetButtons = document.querySelectorAll('.draft-button');
-  saveAsDatasetButtons.forEach((button) =>
-    button.addEventListener('click', () => {
-      postDatasetRequest('http://localhost:5000', 'draft');
-    })
-  );
-
-  function getCookie(name) {
-    const cookieArray = document.cookie.split(';');
-    for (let cookie of cookieArray) {
-      const cookiePair = cookie.split('=');
-      if (name === cookiePair[0].trim()) {
-        return decodeURIComponent(cookiePair[1]);
-      }
-    }
-    return null;
-  }
+  // function getCookie(name) {
+  //   const cookieArray = document.cookie.split(';');
+  //   for (let cookie of cookieArray) {
+  //     const cookiePair = cookie.split('=');
+  //     if (name === cookiePair[0].trim()) {
+  //       return decodeURIComponent(cookiePair[1]);
+  //     }
+  //   }
+  //   return null;
+  // }
 
   // api calls
 
+  function generateFormDataForPostingMetaData(state) {
+    let metaData = new FormData();
+    metaData.append('title', dataset.name);
+    metaData.append('license_id', dataset.license.licenseName);
+    metaData.append('private', dataset.viewPermission === 'Anyone' ? false : true);
+    metaData.append('publisher_type', dataset.publisher.type);
+    metaData.append('start_month', `${dataset.timePeriod.from.year}-${dataset.timePeriod.from.month}`);
+    metaData.append('end_month', `${dataset.timePeriod.to.year}-${dataset.timePeriod.to.month}`);
+    metaData.append('region', [dataset.region]);
+    dataset.publisher.authors.forEach((author, index) => {
+      metaData.append(`publisher_contacts-${index + 1}-name`, author.authorName);
+      metaData.append(`publisher_email-${index + 1}-name`, author.authorEmail);
+    });
+    dataset.referenceLinks.forEach((referenceLink, index) => {
+      metaData.append(`links-${index + 1}-link`, referenceLink.link);
+      metaData.append(`links-${index + 1}-title`, referenceLink.title);
+    });
+    metaData.append('language', dataset.language);
+    metaData.append('tag_string', dataset.keywords.join(','));
+    metaData.append('source', dataset.sources.join(','));
+    metaData.append('dataset_state', state);
+
+    return metaData;
+  }
+
   function postDatasetRequest(baseUrl, state = 'active') {
-
-  var formdata = new FormData();
-  formdata.append("title", dataset.name);
-  formdata.append("license_id", dataset.license.licenseName);
-  formdata.append("private", dataset.viewPermission === 'Anyone' ? false : true);
-  formdata.append("publisher_type", dataset.publisher.type);
-  formdata.append("language", dataset.language);
-  formdata.append("tag_string", dataset.keywords.join(','));
-  formdata.append("source", dataset.sources.join(','));
-  formdata.append("dataset_state", state);
-
-
     fetch(`${baseUrl}/api/dataset/new`, {
       method: 'POST',
       credentials: 'same-origin',
-      body: formdata
+      body: generateFormDataForPostingMetaData(state)
     })
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        var new_formdata = new FormData();
-        new_formdata.append("upload", dataset.files[0].file);
-        new_formdata.append("name", dataset.files[0].fileName);
-        new_formdata.append("description", dataset.files[0].fileDescription);
+        const filesData = new FormData();
+        filesData.append('upload', dataset.files[0].file);
+        filesData.append('name', dataset.files[0].fileName);
+        filesData.append('description', dataset.files[0].fileDescription);
         fetch(`${baseUrl}/api/dataset/${data.pkg_name}/resource/new`, {
           method: 'POST',
           credentials: 'same-origin',
-          body: new_formdata
+          body: filesData
         })
           .then((response) => response.json())
           .then((data) => console.log(data))
