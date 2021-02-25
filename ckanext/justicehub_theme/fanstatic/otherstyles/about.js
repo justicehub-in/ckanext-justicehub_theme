8ea81a817016611c22163017543bd54cc64c1a44
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2021-02-25 11:52:24 +0530

    updated about page

--- ckanext/justicehub_theme/fanstatic/otherstyles/about.js
+++ ckanext/justicehub_theme/fanstatic/otherstyles/about.js
@@ -1,15 +1,11 @@
 const members = [
+  { name: 'Abhinav Singh', image: '/assets/team/abhinav.png' },
   { name: 'Akhil Sagiraju', image: '/assets/team/akhil.jpeg' },
   { name: 'Apoorv Anand', image: '/assets/team/apoorv.png' },
-  { name: 'Charlie Vincent', image: '/assets/team/charlie.jpg' },
-  { name: 'Deepthi Chand', image: '/assets/team/dc.jpg' },
-  { name: 'Divya Rani', image: '/assets/team/divya.jpg' },
   { name: 'Gaurav Godhwani', image: '/assets/team/gaurav.jpg' },
   { name: 'Saurabh Karn', image: '/assets/team/saurabh.jpg' },
-  { name: 'Saurav Roy', image: '/assets/team/saurav.jpeg' },
   { name: 'Shashank Sharma', image: '/assets/team/shashank.jpg' },
   { name: 'Supriya Sankaran', image: '/assets/team/supriya.jpg' },
-  { name: 'Swati Jaiswal', image: '/assets/team/swati.jpg' },
   { name: 'Varun Hemachandran', image: '/assets/team/varun.jpg' }
 ];
 
